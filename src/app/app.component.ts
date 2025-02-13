47037916cde8974666cb95fe40fce169874c6027
Gupta, Sunny <sunnyvales789@gmail.com>
2025-02-13 10:39:19 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class AppComponent {
+  routes: Routes = [];
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    // Fetch the routes from the router
+    this.routes = this.router.config;
+    console.log(this.routes);
+  }
 }
